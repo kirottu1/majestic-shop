1ba1632c07f046e3384b38b124602babd8518a2c
BceHaxep <coolmaster414@gmail.com>
2023-09-05 17:23:05 +0100

    stock page

--- test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var links = $('.bound');
-    var divs = $('div');
-    links.click(function(event){
-        divs.hide();
-        divs.filter('.' + event.target.id).show();
-    });
-});
