30561e4ad2a81e09c4dc59338a8c21aa45d2ac29
BceHaxep <coolmaster414@gmail.com>
2023-09-06 19:30:39 +0100

    price slider rdy

--- scripts/stock.js
+++ scripts/stock.js
@@ -25,56 +25,17 @@ function adjustPosts(post) {
 
 function getPostHTML(data) {
     const modelRegex = /{{model}}/g;
+    const priceRegex = /{{price}}/g;
     return cardTemplate
         .replace(modelRegex, data.model)
+        .replace(priceRegex, data.price)
         .replace('{{id}}', data.id)
         .replace('{{design}}', data.design)
         .replace('{{image}}', data.image)
-        .replace('{{price}}', data.price)
         .replace('{{link}}', data.link)
         .replace('{{brand}}', data.brand);
 }
 //
-// function initializeFiltering() {
-//     const brandCheckboxes = document.querySelectorAll(SELECTOR.BRAND_CHECKBOXES);
-//     const modelCheckboxes = document.querySelectorAll(SELECTOR.MODEL_CHECKBOXES);
-//
-//     function updateModelsVisibility() {
-//         const selectedBrands = Array.from(brandCheckboxes)
-//             .filter((checkbox) => checkbox.checked)
-//             .map((checkbox) => checkbox.id);
-//
-//         const selectedModels = Array.from(modelCheckboxes)
-//             .filter((checkbox) => checkbox.checked)
-//             .map((checkbox) => checkbox.getAttribute('data-model'));
-//
-//         const divs = document.querySelectorAll('.filterDiv');
-//
-//         divs.forEach((div) => {
-//             const brand = div.getAttribute('data-brand');
-//             const model = div.getAttribute('data-model');
-//
-//             const brandIncluded = selectedBrands.length === 0 || selectedBrands.includes(brand);
-//             const modelIncluded = selectedModels.length === 0 || selectedModels.includes(model);
-//
-//             if (brandIncluded && modelIncluded) {
-//                 div.style.display = 'inline-block';
-//             } else {
-//                 div.style.display = 'none';
-//             }
-//         });
-//     }
-//
-//     brandCheckboxes.forEach((checkbox) => {
-//         checkbox.addEventListener('change', updateModelsVisibility);
-//     });
-//
-//     modelCheckboxes.forEach((checkbox) => {
-//         checkbox.addEventListener('change', updateModelsVisibility);
-//     });
-//
-//     updateModelsVisibility();
-// }
 
 function initializeFiltering() {
     const brandCheckboxes = document.querySelectorAll(SELECTOR.BRAND_CHECKBOXES);
