9ffa645e605f7f9329a513c6635a314fa08d8949
BceHaxep <coolmaster414@gmail.com>
2023-09-09 23:45:18 +0100

    OMG LOGIN

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 const bcrypt = require('bcrypt');
-const path = require('path'); // Add this line to use path module
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = '1abc3222748541707846970b016a2d45f13507597ded2b54d87a72961c5cf18f';
+
+const dbPath = path.join(__dirname, 'employees-server', 'db.json');
+const db = JSON.parse(fs.readFileSync(dbPath));
+
 
 app.use(express.json()); // Parse JSON request bodies
 
 // Serve your HTML, CSS, and JavaScript files from the root folder
 app.use(express.static(__dirname)); // Serve files from the root folder
+
+
+passport.use(
+    new LocalStrategy(
+        { usernameField: 'email', passwordField: 'password' },
+        (email, password, done) => {
+            // Find the user by email
+            const user = db.users.find((user) => user.email === email);
+
+            // If the user is not found or the password is incorrect, return an error
+            if (!user || !bcrypt.compareSync(password, user.password)) {
+                return done(null, false, { message: 'Incorrect email or password' });
+            }
+
+            // Authentication successful, return the user
+            return done(null, user);
+        }
+    )
+);
+
+// Passport JWT Strategy for protected routes
+passport.use(
+    new JwtStrategy(
+        {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: JWT_SECRET,
+        },
+        (jwtPayload, done) => {
+            // Find the user by ID
+            const user = db.users.find((user) => user.id === jwtPayload.sub);
+
+            // If the user is found, return the user
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        }
+    )
+);
+
+app.use(passport.initialize());
 
 // Handle user registration
 
@@ -52,26 +105,32 @@ app.post('/register', async (req, res) => {
 });
 
 // Handle user login
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal server error' });
+        }
 
-    // Define the path to the db.json file
-    const dbPath = path.join(__dirname, 'employees-server', 'db.json');
+        if (!user) {
+            return res.status(401).json({ message: info.message });
+        }
 
-    // Load the existing database
-    const db = JSON.parse(fs.readFileSync(dbPath));
+        // Check if the provided password is correct
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({ message: 'Incorrect email or password' });
+        }
 
-    // Find the user with the matching email
-    const user = db.users.find(user => user.email === email);
+        // Generate JWT token
+        const token = jwt.sign({ sub: user.id }, JWT_SECRET);
 
-    // If the user is not found, or the password doesn't match, return an error
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ message: 'Login failed. Please check your credentials.' });
-    }
+        return res.json({ token });
+    })(req, res, next);
+});
 
-    // Authentication successful, you can now create a session or JWT token for the user
-    // Here, I'll simply respond with a success message
-    res.json({ message: 'Login successful' });
+
+app.get('/personal-account', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // If the user reaches this route, it means they are authenticated
+    res.json({ message: 'You have access to your personal account.' });
 });
 
 // Start the server
