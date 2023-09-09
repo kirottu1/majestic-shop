aa9734498b71ed731da56b6b2e1205ea9325bfa0
BceHaxep <coolmaster414@gmail.com>
2023-09-09 22:28:50 +0100

    omg register

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 const path = require('path'); // Add this line to use path module
 
 app.use(express.json()); // Parse JSON request bodies
@@ -9,50 +10,68 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(express.static(__dirname)); // Serve files from the root folder
 
 // Handle user registration
-app.post('/register', (req, res) => {
-    const newUser = req.body; // Assuming your registration form sends a JSON request
-    // Define the path to the db.json file
-    const dbPath = path.join(__dirname, 'employees-server', 'db.json');
 
-    // Load the existing database
-    const db = JSON.parse(fs.readFileSync(dbPath));
+app.post('/register', async (req, res) => {
+    const newUser = req.body;
 
-    const existingUser = db.users.find((user) => user.email === newUser.email);
+    // Generate a salt and hash the user's password
+    try {
+        const saltRounds = 10; // Adjust the number of rounds as needed for your application
+        const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
 
-    if (existingUser) {
-        // Email already exists; send an error response
-        return res.status(400).json({ message: 'Email already registered. Please choose a different email.' });
-    } else {
-        // Email is unique; proceed with registration
+        // Create a new user object with the hashed password
+        const userWithHashedPassword = {
+            ...newUser,
+            password: hashedPassword,
+        };
+
+        // Define the path to the db.json file
+        const dbPath = path.join(__dirname, 'employees-server', 'db.json');
+
+        // Load the existing database
+        const db = JSON.parse(fs.readFileSync(dbPath));
+
+        // Check if the email already exists in the database
+        const existingUser = db.users.find(user => user.email === newUser.email);
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already registered' });
+        }
+
         // Add the new user to the database
-        db.users.push(newUser);
+        db.users.push(userWithHashedPassword);
 
         // Update the db.json file with the new user data
         fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
 
         // Respond with a success message
         res.json({ message: 'Registration successful' });
-        console.log('New user registered:', newUser);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Registration failed' });
     }
 });
-// Handle user login
-app.post('/login', (req, res) => {
-    const { email, password } = req.body; // Get user input from request
 
-    // Load the database
+// Handle user login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Define the path to the db.json file
     const dbPath = path.join(__dirname, 'employees-server', 'db.json');
+
+    // Load the existing database
     const db = JSON.parse(fs.readFileSync(dbPath));
 
-    // Find the user in the database
-    const user = db.users.find((u) => u.email === email && u.password === password);
+    // Find the user with the matching email
+    const user = db.users.find(user => user.email === email);
 
-    if (user) {
-        // User is authenticated, you can send a success message
-        res.json({ message: 'Login successful' });
-    } else {
-        // User authentication failed, send an error message
-        res.status(401).json({ message: 'Login failed. Please check your credentials.' });
+    // If the user is not found, or the password doesn't match, return an error
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: 'Login failed. Please check your credentials.' });
     }
+
+    // Authentication successful, you can now create a session or JWT token for the user
+    // Here, I'll simply respond with a success message
+    res.json({ message: 'Login successful' });
 });
 
 // Start the server
