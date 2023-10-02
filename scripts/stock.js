d44b4711f7159874c36d36aefdb549f7bc2b3187
BceHaxep <coolmaster414@gmail.com>
2023-10-02 21:55:57 +0100

    really good code

--- scripts/stock.js
+++ scripts/stock.js
@@ -13,6 +13,7 @@ function init() {
         .then((res) => {
             adjustPosts(res);
             initializeFiltering();
+            initialiseLinks();
         });
 }
 
@@ -26,10 +27,11 @@ function adjustPosts(post) {
 function getPostHTML(data) {
     const modelRegex = /{{model}}/g;
     const priceRegex = /{{price}}/g;
+    const idRegex = /{{id}}/g;
     return cardTemplate
         .replace(modelRegex, data.model)
         .replace(priceRegex, data.price)
-        .replace('{{id}}', data.id)
+        .replace(idRegex, data.id)
         .replace('{{design}}', data.design)
         .replace('{{image}}', data.image)
         .replace('{{link}}', data.link)
@@ -97,4 +99,117 @@ function initializeFiltering() {
 
     updateModelsVisibility();
 }
+
+function initialiseLinks(){
+    const cardLinks = document.querySelectorAll(".card-details-link");
+
+    cardLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault(); // Prevent the default link behavior
+
+            // Get the data-id attribute of the clicked card
+            const itemId = link.closest(".card").getAttribute("data-id");
+
+            // Construct the URL for the item detail page (e.g., item.ejs)
+            const itemDetailUrl = `/item/${itemId}`;
+
+            // Redirect to the item detail page
+            window.location.href = itemDetailUrl;
+        });
+    });
+}
+
 ///
+const config = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect,
+        pool: {
+            max: config.pool.max,
+            min: config.pool.min,
+            acquire: config.pool.acquire,
+            idle: config.pool.idle
+        }
+    }
+);
+
+// const db = {};
+//
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
+//
+// db.User = require("../models/user.model.js")(sequelize, Sequelize);
+// db.Role = require("../models/role.model.js")(sequelize, Sequelize);
+// db.OrderDetails = require("../models/order-details.model.js")(sequelize, Sequelize);
+// db.OrderItems = require("../models/order-items.model.js")(sequelize, Sequelize);
+// db.PaymentDetails = require("../models/payment-details.model.js")(sequelize, Sequelize);
+// db.Product = require("../models/product.model.js")(sequelize, Sequelize);
+// db.Discount = require("../models/discount.model.js")(sequelize, Sequelize);
+// db.CartItem = require("../models/cart-item.model.js")(sequelize, Sequelize);
+// db.ShoppingSession = require("../models/shopping-session.model.js")(sequelize, Sequelize);
+//
+// // Define associations here
+// db.Role.belongsToMany(db.User, {
+//     through: "user_roles",
+// });
+// db.User.belongsToMany(db.Role, {
+//     through: "user_roles",
+// });
+// db.OrderDetails.belongsTo(db.User, {
+//     foreignKey: "OrderUserID",
+//     as: "user",
+// });
+// db.User.hasMany(db.OrderDetails, {
+//     foreignKey: "OrderUserID",
+//     as: "order_details",
+// });
+// db.OrderDetails.hasMany(db.OrderItems, {
+//     as: "order_items",
+// });
+// db.OrderDetails.hasOne(db.PaymentDetails, {
+//     foreignKey: "order_id",
+//     as: "payment_details",
+// });
+// db.OrderItems.belongsTo(db.Product, {
+//     foreignKey: "product_id",
+//     as: "product",
+// });
+// db.Product.hasMany(db.OrderItems, {
+//     as: "order_items",
+// });
+// db.Product.belongsTo(db.Discount, {
+//     foreignKey: "discount_id",
+//     as: "discount",
+// });
+// db.Discount.hasMany(db.Product, {
+//     as: "products",
+// });
+// db.CartItem.belongsTo(db.Product, {
+//     foreignKey: "product_id",
+//     as: "product",
+// });
+// db.Product.hasOne(db.CartItem, {
+//     foreignKey: "product_id",
+//     as: "cart_item",
+// });
+// db.ShoppingSession.belongsTo(db.User, {
+//     foreignKey: "user_id",
+//     as: "user",
+// });
+// db.User.hasOne(db.ShoppingSession, {
+//     foreignKey: "user_id",
+//     as: "shopping_session",
+// });
+// db.ShoppingSession.hasMany(db.CartItem, {
+//     as: "cart_items",
+// });
+//
+// db.ROLES = ["user", "admin", "moderator"];
+//
+// module.exports = db;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 const path = require("path");
-
+const axios = require('axios');
 const app = express();
 
 app.use(cors());
@@ -46,12 +46,36 @@ function initial() {
     });
 }
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname)));
 
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to kirottu application." });
 });
+
+app.get('/item/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id; // Get the item ID from the URL
+
+        // Make an HTTP GET request to fetch item data based on the itemId
+        const response = await axios.get(`http://localhost:3000/stock?id=${itemId}`);
+        // Extract the item data from the response
+        const item = response.data[0];
+        //
+        console.log(item)
+        console.log(item.id)
+        // // Render the item.ejs view and pass the itemData to it
+        res.render('item', { item });
+    } catch (error) {
+        // Handle any errors here
+        console.error('Error fetching item data:', error);
+        // You can also render an error page or redirect as needed
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
